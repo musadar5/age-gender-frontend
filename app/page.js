95d754ea3fb3ec5f48c16722b12d85b0419b6382
Musa Dar <darmusa572@gmail.com>
2025-05-25 14:46:39 +0500

    some changes made

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
     formData.append("file", file);
 
     setLoading(true);
-    const res = await fetch("https://age-gender-backend-production.up.railway.app/predict", {
+    const res = await fetch("http://127.0.0.1:5000/predict", {
       method: "POST",
       body: formData,
     });
